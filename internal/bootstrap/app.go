7eabb6df822c29ac7badefc192246dac195bee24
Prabhat Rawat <prabhatkumarrawat210@gmail.com>
2026-05-10 01:20:20 +0530

    config library for ENV variable

--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"context"
+	"userAuth/internal/platform/config"
 	"userAuth/internal/platform/logger"
 	"userAuth/internal/platform/postgres"
 
@@ -14,7 +15,13 @@ type App struct {
 
 func Initialize(ctx context.Context) (*App, error) {
 	log := logger.FromContext(ctx)
-	db, err := postgres.NewPool(ctx)
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to load configs")
+		return nil, err
+	}
+	db, err := postgres.NewPool(ctx, cfg.Postgres)
 	if err != nil {
 		log.Error().Msg("Failed to intialize postgres")
 		return nil, err
---
+++ internal/platform/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import "github.com/caarlos0/env"
+
+type Config struct {
+	ServiceName string `env:"SERVICE_NAME" envDefault:"auth-service"`
+	Environment string `env:"ENVIRONMENT" envDefault:"DEV"`
+	Postgres    PostgresConfig
+}
+
+type PostgresConfig struct {
+	Host     string `env:"POSTGRES_HOST" envDefault:"localhost"`
+	Port     int    `env:"POSTGRES_PORT" envDefault:"5432"`
+	User     string `env:"POSTGRES_USER,required"`
+	Password string `env:"POSTGRES_PASSWORD,required"`
+	Database string `env:"POSTGRES_DB,required"`
+	SSLMode  string `env:"POSTGRES_SSL_MODE" envDefault:"disable"`
+
+	MaxConns                int32 `env:"POSTGRES_MAX_CONNS" envDefault:"25"`
+	MinConns                int32 `env:"POSTGRES_MIN_CONNS" envDefault:"5"`
+	MaxConnLifetimeMinute   int   `env:"POSTGRES_MAX_CONN_LIFETIME_MIN" envDefault:"30"`
+	MaxConnIdleTimeMinute   int   `env:"POSTGRES_MAX_CONN_IDLE_TIME_MIN" envDefault:"10"`
+	HealthCheckPeriodMinute int   `env:"POSTGRES_HEALTH_CHECK_PERIOD_MIN" envDefault:"1"`
+	ConnectTimeoutSec       int   `env:"POSTGRES_CONNECT_TIMEOUT_SEC" envDefault:"5"`
+
+	MigrationVersion uint `env:"MIGRATION_VERSION" envDefault:"5"`
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
